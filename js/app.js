ce24ceb9a1bc96fd4963b5842fbf226d85cf0732
Sitora Urazova-Lu <urazova.s@gmail.com>
2021-10-23 12:29:41 -0400

    final

--- js/app.js
+++ js/app.js
@@ -87,66 +87,18 @@ function buildNav() {
 buildNav();
 
 // Add and remove active class for navbar
-// navbar = document.querySelector(".navbar_list").querySelectorAll("a");
-// console.log(navbar);
+navbar = document.querySelectorAll(".nav-link");
+console.log(navbar);
 
-// navbar.forEach((element) => {
-//   element.addEventListener("click", function () {
-//     navbar.forEach((nav) => nav.classList.remove("active"));
+navbar.forEach((element) => {
+  element.addEventListener("click", function () {
+    navbar.forEach((nav) => nav.classList.remove("active"));
 
-//     this.classList.add("active");
-//   });
-// });
+    this.classList.add("active");
+  });
+});
 
-// Add and remove active class for navbar
-// navbar = document.querySelectorAll(".nav-link");
-// console.log(navbar);
-
-// navbar.forEach((element) => {
-//   element.addEventListener("click", function () {
-//     navbar.forEach((nav) => nav.classList.remove("active"));
-
-//     this.classList.add("active");
-//   });
-// });
-
-/////////////////////////////////////////////////////////
-// // // Get all sections that have an ID defined
-// const sectionss = document.querySelectorAll("section");
-// console.log(sectionss);
-
-// // Add an event listener listening for scroll
-// window.addEventListener("scroll", navHighlighter);
-
-// function navHighlighter() {
-//   // Get current scroll position
-//   let scrollY = window.pageYOffset;
-
-//   // Now we loop through sections to get height, top and id values for each
-//   sectionss.forEach((current) => {
-//     const sectionHeight = current.offsetHeight;
-//     // console.log(sectionss);
-
-//     const sectionTop =
-//       current.getBoundingClientRect().top + window.pageYOffset - 500;
-//     sectionId = current.getAttribute("id");
-//     // console.log(sectionTop);
-
-//     /*
-//     - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
-//     - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
-//     */
-//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-//       document.getElementsByClassName(".nav-link").classList.add("active");
-//     } else {
-//       document.querySelectorAll(".nav-link").classList.remove("active");
-//       console.log(scrollY);
-//       console.log(sectionTop);
-//       //   console.log(sectionHeight);
-//     }
-//   });
-// }
-
+// Add active class to navbar item when in viewport while scrolling
 const links = document.querySelectorAll(".nav-link");
 const sectionsss = document.querySelectorAll("section");
 function changeLinkState() {
@@ -160,74 +112,3 @@ function changeLinkState() {
 
 changeLinkState();
 window.addEventListener("scroll", changeLinkState);
-
-// const sectionz = document.querySelectorAll("section");
-// const navLi = document.querySelectorAll(".navbar-item a");
-// window.addEventListener("scroll", () => {
-//   let current = ".nav-link";
-//   sectionz.forEach((section) => {
-//     const sectionTop = section.offsetTop;
-//     const sectionHeight = section.clientHeight;
-//     if (pageYOffset >= sectionTop - sectionHeight / 3) {
-//       current = section.getAttribute("id");
-//     }
-//   });
-
-//   navLi.forEach((li) => {
-//     li.classList.remove("active");
-//     if (li.classList.contains(current)) {
-//       li.classList.add("active");
-//     }
-//   });
-// });
-
-// //take 2
-// const sectionss = document.querySelectorAll("section");
-// const navLi = document.querySelectorAll(".navbar-item a");
-// window.onscroll = () => {
-//   var current = ".nav-link";
-
-//   sectionss.forEach((section) => {
-//     const sectionTop = section.offsetTop;
-//     if (pageYOffset >= sectionTop - 60) {
-//       current = section.getAttribute("class");
-//     }
-//   });
-
-//   navLi.forEach((a) => {
-//     a.classList.remove("active");
-//     if (a.classList.contains(current)) {
-//       a.classList.add("active");
-//     }
-//   });
-// };
-
-// Viewport and Highlight Navbar on Scroll
-// const sectionz = document.querySelectorAll("section");
-// const navLi = document.querySelectorAll(".nav-link");
-// window.addEventListener("scroll", () => {
-//   let current = "";
-//   console.log(current);
-
-//   sectionz.forEach((section) => {
-//     const sectionTop = section.offsetTop;
-//     const sectionHeight = section.clientHeight;
-//     if (pageYOffset >= sectionTop - sectionHeight / 3) {
-//       current = section.getAttribute("id");
-
-//       console.log(current);
-//     }
-//   });
-
-//   navLi.forEach((e) => {
-//     e.classList.remove("active");
-//     if (e.classList.contains(current)) {
-//       e.classList.add("active");
-//       console.log(current);
-//     }
-//   });
-// });
-
-// toggleButton.addEventListener("scroll", () => {
-//   navbarLinks.classList.toggle("active");
-// });
